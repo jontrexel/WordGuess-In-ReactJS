b0c3f1e48ebc7ce0700e85e87b260f458ccba6b3
jontrexel <56047702+jontrexel@users.noreply.github.com>
2022-09-25 11:30:46 -0700

    App.js random word generator, input validation
    Added random word generator, a sixth line (prior version only allowed five guesses, this allows six to be easier), sharing of hidden word to console.log after six tries, and the prevention of entering non-alphabetic characters.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import logo from './w.png';
 import './App.css';
 import React from "react";
 
+
 class WordleRow extends React.Component {
   constructor(props) {
     super(props);
@@ -29,20 +30,29 @@ class WordleRow extends React.Component {
   handleChange (changeIndex, event) {
     // make shallow copy of input array
     let array = [...this.state.inputArray];
-    // modify array item at changeIndex index location
-    array[changeIndex] = event.target.value.toUpperCase();
-    // replace inputArray in state
-    this.setState({
-      inputArray: array
-    })
+    // get character entered
+    const char = event.target.value;
+    // if character is alphabetic
+    if (this.isAlphabetic(char))
+    {
+      // modify array item at changeIndex index location
+      array[changeIndex] = char.toUpperCase();
+      // replace inputArray in state
+      this.setState({
+        inputArray: array
+      });
+      // stores last character entered
+      this.updateTracker = event.target.value.toUpperCase();
+      // move focus to next input if character is entered
+      this.moveFocus(event.target.value);
+    }
     
-    // stores last character entered
-    this.updateTracker = event.target.value.toUpperCase();
-
-    // move focus to next input if character is entered
-    this.moveFocus(event.target.value);
   }
 
+  isAlphabetic(char)
+  {
+    return (char.toLowerCase () !== char.toUpperCase ());
+  }
 
   moveFocus (val) {
     if (val !== "")
@@ -76,13 +86,11 @@ class WordleRow extends React.Component {
     this.setState({correctLetterCount: 0, disabled: true});
 
     // for each element in inputArray
-    this.state.inputArray.map( (currentValue, index) => {
+    this.state.inputArray.forEach( (currentValue, index) => {
       
       // if entered letter is in wordleArray, change to yellow
       if (this.props.wordleArray.includes(currentValue))
         this.changeColor(index, "yellow");
-      else
-        this.changeColor(index, "#4e5156");
 
       // if entered letter is in same position in wordleArray, change to green
       if (currentValue === this.props.wordleArray[index]) {
@@ -139,10 +147,52 @@ class WordleTable extends React.Component {
 
     super();
 
-    this.wordleRows = [];
+    //this.word = "REACT";
+    this.word_list = ['about', 'above', 'abuse', 'actor', 'acute', 'admit', 'adopt', 'adult', 'after', 'again',
+    'agent', 'agree', 'ahead', 'alarm', 'album', 'alert', 'alike', 'alive', 'allow', 'alone', 'along', 'alter',
+    'among', 'anger', 'Angle', 'angry', 'apart', 'apple', 'apply', 'arena', 'argue', 'arise', 'array', 'aside', 
+    'asset', 'audio', 'audit', 'avoid', 'award', 'aware', 'badly', 'baker', 'bases', 'basic', 'basis', 'beach', 
+    'began', 'begin', 'begun', 'being', 'below', 'bench', 'billy', 'birth', 'black', 'blame', 'blind', 'block', 
+    'blood', 'board', 'boost', 'booth', 'bound', 'brain', 'brand', 'bread', 'break', 'breed', 'brief', 'bring', 
+    'broad', 'broke', 'brown', 'build', 'built', 'buyer', 'cable', 'calif', 'carry', 'catch', 'cause', 'chain', 
+    'chair', 'chart', 'chase', 'cheap', 'check', 'chest', 'chief', 'child', 'china', 'chose', 'civil', 'claim', 
+    'class', 'clean', 'clear', 'click', 'clock', 'close', 'coach', 'coast', 'could', 'count', 'court', 'cover', 
+    'craft', 'crash', 'cream', 'crime', 'cross', 'crowd', 'crown', 'curve', 'cycle', 'daily', 'dance', 'dated', 
+    'dealt', 'death', 'debut', 'delay', 'depth', 'doing', 'doubt', 'dozen', 'draft', 'drama', 'drawn', 'dream', 
+    'dress', 'drill', 'drink', 'drive', 'drove', 'dying', 'eager', 'early', 'earth', 'eight', 'elite', 'empty', 
+    'enemy', 'enjoy', 'enter', 'entry', 'equal', 'error', 'event', 'every', 'exact', 'exist', 'extra', 'faith', 
+    'false', 'fault', 'fiber', 'field', 'fifth', 'fifty', 'fight', 'final', 'first', 'fixed', 'flash', 'fleet', 
+    'floor', 'fluid', 'focus', 'force', 'forth', 'forty', 'forum', 'found', 'frame', 'frank', 'fraud', 'fresh', 
+    'front', 'fruit', 'fully', 'funny', 'giant', 'given', 'glass', 'globe', 'going', 'grace', 'grade', 'grand', 
+    'grant', 'grass', 'great', 'green', 'gross', 'group', 'grown', 'guard', 'guess', 'guest', 'guide', 'happy', 
+    'harry', 'heart', 'heavy', 'hence', 'henry', 'horse', 'hotel', 'house', 'human', 'ideal', 'image', 'index', 
+    'inner', 'input', 'issue', 'japan', 'jimmy', 'joint', 'jones', 'judge', 'known', 'label', 'large', 'laser', 
+    'later', 'laugh', 'layer', 'learn', 'lease', 'least', 'leave', 'legal', 'level', 'lewis', 'light', 'limit', 
+    'links', 'lives', 'local', 'logic', 'loose', 'lower', 'lucky', 'lunch', 'lying', 'magic', 'major', 'maker', 
+    'march', 'maria', 'match', 'maybe', 'mayor', 'meant', 'media', 'metal', 'might', 'minor', 'minus', 'mixed', 
+    'model', 'money', 'month', 'moral', 'motor', 'mount', 'mouse', 'mouth', 'movie', 'music', 'needs', 'never', 
+    'newly', 'night', 'noise', 'north', 'noted', 'novel', 'nurse', 'occur', 'ocean', 'offer', 'often', 'order', 
+    'other', 'ought', 'paint', 'panel', 'paper', 'party', 'peace', 'peter', 'phase', 'phone', 'photo', 'piece', 
+    'pilot', 'pitch', 'place', 'plain', 'plane', 'plant', 'plate', 'point', 'pound', 'power', 'press', 'price', 
+    'pride', 'prime', 'print', 'prior', 'prize', 'proof', 'proud', 'prove', 'queen', 'quick', 'quiet', 'quite', 
+    'radio', 'raise', 'range', 'rapid', 'ratio', 'reach', 'ready', 'refer', 'right', 'rival', 'river', 'robin', 
+    'roger', 'roman', 'rough', 'round', 'route', 'royal', 'rural', 'scale', 'scene', 'scope', 'score', 'sense', 
+    'serve', 'seven', 'shall', 'shape', 'share', 'sharp', 'sheet', 'shelf', 'shell', 'shift', 'shirt', 'shock', 
+    'shoot', 'short', 'shown', 'sight', 'since', 'sixth', 'sixty', 'sized', 'skill', 'sleep', 'slide', 'small', 
+    'smart', 'smile', 'smith', 'smoke', 'solid', 'solve', 'sorry', 'sound', 'south', 'space', 'spare', 'speak', 
+    'speed', 'spend', 'spent', 'split', 'spoke', 'sport', 'staff', 'stage', 'stake', 'stand', 'start', 'state', 
+    'steam', 'steel', 'stick', 'still', 'stock', 'stone', 'stood', 'store', 'storm', 'story', 'strip', 'stuck', 
+    'study', 'stuff', 'style', 'sugar', 'suite', 'super', 'sweet', 'table', 'taken', 'taste', 'taxes', 'teach', 
+    'teeth', 'terry', 'texas', 'thank', 'theft', 'their', 'theme', 'there', 'these', 'thick', 'thing', 'think', 
+    'third', 'those', 'three', 'threw', 'throw', 'tight', 'times', 'tired', 'title', 'today', 'topic', 'total', 
+    'touch', 'tough', 'tower', 'track', 'trade', 'train', 'treat', 'trend', 'trial', 'tried', 'tries', 'truck', 
+    'truly', 'trust', 'truth', 'twice', 'under', 'undue', 'union', 'unity', 'until', 'upper', 'upset', 'urban', 
+    'usage', 'usual', 'valid', 'value', 'video', 'virus', 'visit', 'vital', 'voice', 'waste', 'watch', 'water', 
+    'wheel', 'where', 'which', 'while', 'white', 'whole', 'whose', 'woman', 'women', 'world', 'worry', 'worse', 
+    'worst', 'worth', 'would', 'wound', 'write'];
 
-    this.word = "REACT";
-    this.wordleArray = Array.from(this.word)
+    this.word = this.word_list[Math.round(Math.random() * this.word_list.length)];
+    this.wordleArray = Array.from(this.word.toUpperCase());
     
     this.state = {
       wordleArray: this.wordleArray,
@@ -170,7 +220,7 @@ class WordleTable extends React.Component {
     enableRowsCopy[thisIndex] = 0;
 
     // if user hasn't lost yet (five tries not yet reached), play next row:
-    if (thisIndex < 4)
+    if (thisIndex < 5)
     {
       // display next row
       displayRowsCopy[thisIndex + 1 ] = 1;
@@ -198,7 +248,7 @@ class WordleTable extends React.Component {
         {
           currentRow: thisRow + 1,
         },
-        () => { console.log("Game Over! Five tries reached.") }
+        () => { console.log("Game Over! Six tries reached. Secret word was: " + this.word + ".") }
       );
     }
   }
@@ -227,10 +277,11 @@ class WordleTable extends React.Component {
             { (this.state.displayRows[2] === 1) ? <WordleRow wordleArray={this.state.wordleArray} currentRow={this.state.currentRow} incrementCurrentRow={this.incrementCurrentRow} winningRow={this.state.winningRow} setWinningRow={this.setWinningRow} displayRows={this.state.displayRows} enableRows={this.state.enableRows} /> : "" }
             { (this.state.displayRows[3] === 1) ? <WordleRow wordleArray={this.state.wordleArray} currentRow={this.state.currentRow} incrementCurrentRow={this.incrementCurrentRow} winningRow={this.state.winningRow} setWinningRow={this.setWinningRow} displayRows={this.state.displayRows} enableRows={this.state.enableRows} /> : "" }
             { (this.state.displayRows[4] === 1) ? <WordleRow wordleArray={this.state.wordleArray} currentRow={this.state.currentRow} incrementCurrentRow={this.incrementCurrentRow} winningRow={this.state.winningRow} setWinningRow={this.setWinningRow} displayRows={this.state.displayRows} enableRows={this.state.enableRows} /> : "" }
+            { (this.state.displayRows[5] === 1) ? <WordleRow wordleArray={this.state.wordleArray} currentRow={this.state.currentRow} incrementCurrentRow={this.incrementCurrentRow} winningRow={this.state.winningRow} setWinningRow={this.setWinningRow} displayRows={this.state.displayRows} enableRows={this.state.enableRows} /> : "" }
           </tbody>
         </table>
         <h5>{ (this.state.winningRow) ? "You Won! Attempts: " + this.state.winningRow : "" }</h5>
-        <h5>{ (this.state.currentRow > 5) ? "Game Over! Five tries reached." : "" }</h5>
+        <h5>{ (this.state.currentRow > 6) ? "Game Over! Six tries reached." : "" }</h5>
       </div>
     )
   }
